6677f41f6b91d140fff428023e500b862efdbfe9
Tiago A. de Oliveira <tsabian@hotmail.com>
2021-09-24 21:05:13 -0300

    Initial Commit

---
+++ SwiftMock/SwiftMock.h
@@ -0,0 +1,18 @@
+//
+//  SwiftMock.h
+//  SwiftMock
+//
+//  Created by Tiago Oliveira on 24/09/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftMock.
+FOUNDATION_EXPORT double SwiftMockVersionNumber;
+
+//! Project version string for SwiftMock.
+FOUNDATION_EXPORT const unsigned char SwiftMockVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftMock/PublicHeader.h>
+
+
